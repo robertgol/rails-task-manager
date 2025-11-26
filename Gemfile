89ea6ea3a344cd07431b7a3ca5386f6d673a77bd
Robert Golusinski <robertgolusinski@gmail.com>
2025-11-26 15:16:19 +0100

    implemented manual crud with simple views

--- Gemfile
+++ Gemfile
@@ -62,3 +62,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "standard", "~> 1.52"
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get "tasks/:id/edit" => "tasks#edit", as: :edit_task
+  patch "tasks/:id" => "tasks#update"
+
+  get "tasks/new" => "tasks#new", as: :new_task
+  post "tasks" => "tasks#create"
+
+  get "tasks" => "tasks#index"
+  get "tasks/:id" => "tasks#show", as: :task
+
+  delete "tasks/:id" => "tasks#destroy"
 end
