5a1fa9068f6b5bc75703f36593f611ad4ee2f7a6
Robert Golusinski <robertgolusinski@gmail.com>
2025-11-26 12:33:49 +0100

    generated tasks model

---
+++ db/migrate/20251125155736_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :details
+      t.boolean :completed, default: false, null: false
+
+      t.timestamps
+    end
+
+    add_index :tasks, :completed
+  end
+end
